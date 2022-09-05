4805eccc4c77c624861b39e1466cad0e5d5112fc
vincentvigon <vincent.vigon@gmail.com>
2022-09-05 15:57:26 +0200

    python path

--- __init__.py
+++ __init__.py
@@ -1,3 +1,8 @@
+import sys
+"ajouter un chemin au début de la liste"
+
+sys.path.insert(0, "small_libs")
+
 from consecutive_windows import make_consecutive_windows
 
-print("importation de small_libs")
+print("importation de small_libs, ajout de small_libs dans votre PYTHONPATH")
