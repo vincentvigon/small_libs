c534d00649d54168ab3b0b4727f69f9a6a270b81
vincentvigon <vincent.vigon@gmail.com>
2022-09-05 16:13:11 +0200

    sl import

--- __init__.py
+++ __init__.py
@@ -1,16 +1,14 @@
-import tensorflow as tf
-import matplotlib.pyplot as plt
-import numpy as np
-
 import sys
 "ajouter un chemin au début de la liste"
 
-sys.path.insert(0, "small_libs")
+#sys.path.insert(0, "small_libs")
 
 print("importation de small_libs, ajout de small_libs dans votre PYTHONPATH")
-
+print("small_libs importation")
 
 from consecutive_windows.make_consecutive_windows import  make_consecutive_windows
+from derivator_fft.derivator import Derivator_fft
+from derivator_fft.fft_nd import Fft_nd, fft_nd, ifft_nd
 
 
 
